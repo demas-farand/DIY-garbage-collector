aefdea44cfe7f23a5a6ef197d70e6af27a49752d
demas-farand <demas.farand@gmail.com>
2026-06-03 22:13:38 +0700

    refactor: view model -Update bind data views and models

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.ObjectModel;
+using System.Linq;
 using CustomGarbageCollector.Models;
 using CustomGarbageCollector.Services;
 
@@ -7,36 +9,66 @@ namespace CustomGarbageCollector.ViewModels;
 public class MainWindowViewModel : ViewModelBase
 {
     private readonly BlackHoleService _blackHoleService;
+    private double _distortionLevel;
+    public double DistortionLevel
+    {
+        get => _distortionLevel;
+        set => RaiseAndSetIfChanged(ref _distortionLevel, value);
+    }
+
+    public ObservableCollection<SpaceObject> SpaceObjects { get; }
 
     public MainWindowViewModel()
     {
         _blackHoleService = new BlackHoleService();
-        
-        RunCosmicSimulation();
+        SpaceObjects = new ObservableCollection<SpaceObject>();
+        DistortionLevel = 0;
     }
 
-    private void RunCosmicSimulation()
+    public void SpawnPlanet()
     {
-        Console.WriteLine("\nSTARTING BLACK HOLE GC SIMULATION");
-        
-        var earth = new DataPlanet("Earth", 1000);
-        var mars = new DataPlanet("Mars", 800);
-        var spaceDust = new CacheAsteroid(50);
-        var alienShip = new CacheAsteroid(120);
-        
-        _blackHoleService.SpawnObject(earth);
-        _blackHoleService.SpawnObject(mars);
-        _blackHoleService.SpawnObject(spaceDust);
-        _blackHoleService.SpawnObject(alienShip);
-        
-        _blackHoleService.PrintUniverseStatus();
+        var planet = new DataPlanet("Planet", 10);
+        _blackHoleService.SpawnObject(planet);
+        SpaceObjects.Add(planet);
+        Console.WriteLine($"[UI] Spawned Planet. Total objects: {SpaceObjects.Count}");
+    }
 
-        Console.WriteLine("\n[Simulation] Releasing references from Earth and Space Dust...");
-        earth.MarkAsGarbage();
-        spaceDust.MarkAsGarbage();
+    public void SpawnAsteroid()
+    {
+        var asteroid = new CacheAsteroid(2);
+        _blackHoleService.SpawnObject(asteroid);
+        SpaceObjects.Add(asteroid);
+        Console.WriteLine($"[UI] Spawned Asteroid. Total objects: {SpaceObjects.Count}");
+    }
+
+    public void MarkAsGarbage()
+    {
+        var activeObject = SpaceObjects.FirstOrDefault(o => !o.IsGarbage);
+        if (activeObject != null)
+        {
+            activeObject.MarkAsGarbage();
+            Console.WriteLine($"[UI] Marked {activeObject.Id} as garbage.");
+        }
+        else
+        {
+            Console.WriteLine("[UI] No active objects left to mark as garbage.");
+        }
+    }
+
+    public void ActivateBlackHole()
+    {
         _blackHoleService.SuckGarbage();
+
+        var garbageList = SpaceObjects.Where(o => o.IsGarbage).ToList();
+        foreach (var garbage in garbageList)
+        {
+            SpaceObjects.Remove(garbage);
+        }
+
+        DistortionLevel += garbageList.Count * 5; 
         
-        _blackHoleService.PrintUniverseStatus();
-        Console.WriteLine("SIMULATION COMPLETE\n");
+        if (DistortionLevel > 100) DistortionLevel = 100;
+        
+        Console.WriteLine($"[UI] Sweep complete. Current Distortion: {DistortionLevel}%");
     }
 }
